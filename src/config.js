050d7183a14940a6889dd1fffd2d3e66d4126e09
yaswanth <kumaryaswanth557@gmail.com>
2026-04-03 15:05:50 +0530

    added new backend url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = ['localhost', '127.0.0.1'].includes(window.location.hostname)
     ? 'http://localhost:5000' 
-    : 'https://culture-connect-tt2l.onrender.com';
+    : 'https://cultureconnect-c1fm.onrender.com';
